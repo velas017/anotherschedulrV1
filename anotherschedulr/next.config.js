9b1e04b90ec31d9e0a03f5819ca1747e42b2db47
velas017 <128103668+velas017@users.noreply.github.com>
2025-07-01 19:03:25 -0400

    initial project commit the beginning of it all

---
+++ anotherschedulr/next.config.js
@@ -0,0 +1,18 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false,
+        };
+      }
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
