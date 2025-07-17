018206ea496c37c781bb90712f3bbe713ab8b609
velas017 <128103668+velas017@users.noreply.github.com>
2025-07-17 13:34:34 -0400

    created a client page with a table and search feature for the users clients

--- anotherschedulr/src/app/dashboard/page.tsx
+++ anotherschedulr/src/app/dashboard/page.tsx
@@ -10,7 +10,6 @@ import {
   FileText, 
   Settings,
   Bell,
-  Search,
   ChevronRight,
   Clock,
   DollarSign,
@@ -311,16 +310,6 @@ const DashboardPage = () => {
               </div>
               
               <div className="flex items-center space-x-4">
-                {/* Search */}
-                <div className="relative hidden md:block">
-                  <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
-                  <input
-                    type="text"
-                    placeholder="Search..."
-                    className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                  />
-                </div>
-                
                 {/* Notifications */}
                 <button className="relative p-2 rounded-lg hover:bg-gray-100">
                   <Bell className="w-5 h-5 text-gray-600" />
